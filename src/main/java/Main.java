cb7b7e9be6fb8b97233316c2afc4204708dc0971
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2018-07-08 15:04:56 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+	static final Logger LOGGER = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello, Logger");
+	}
+}
